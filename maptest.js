ccc75b4aa3f3b8f27d0861c3dff4dbed417945ab
Code-name-185 <valentin.mulder@unil.ch>
2021-05-01 10:41:35 +0200

    Update des doonnées

--- maptest.js
+++ maptest.js
@@ -21,12 +21,15 @@ svg.call(d3.zoom().on('zoom', () => {
     g.attr('transform', d3.event.transform);
 }));
 
-const colorScale =d3.scaleOrdinal(d3.schemeCategory10);
+const colorScale =d3.scaleOrdinal();
+
+const colorValueGDP = d => d.properties.GDP;
+const colorValueOECD = d => d.properties.OECD;
 
 Promise.all([
     d3.json('https://unpkg.com/world-atlas@1.1.4/world/50m.json'),
     d3.csv("https://raw.githubusercontent.com/Code-name-185/Visulalisation-de-donnees-SP2021/main/country.csv"),
-    d3.csv("https://raw.githubusercontent.com/Code-name-185/Visulalisation-de-donnees-SP2021/main/Party_data.csv")
+    d3.csv("https://raw.githubusercontent.com/Code-name-185/Visulalisation-de-donnees-SP2021/main/party.csv")
 ]).then(([topoJsonData,  csvDataC, csvDataP]) => {
     
     const rowByCNameC = {};
@@ -41,7 +44,7 @@ Promise.all([
         rowByPNameP[d.Partyname] = d;
     });
 
-console.log(rowByCNameC);
+//console.log(rowByCNameC);
 //console.log(rowByPNameP);
 
 const countries = topojson.feature(topoJsonData, topoJsonData.objects.countries);
@@ -50,11 +53,16 @@ const countries = topojson.feature(topoJsonData, topoJsonData.objects.countries)
         Object.assign(d.properties, rowByCNameC[d.id], rowByPNameP[d.id]);
     });
 
+    colorScale
+        .domain(countries.features.map(colorValueOECD))
+        .domain(colorScale.domain().sort().reverse())
+        .range(d3.schemeSpectral[colorScale.domain().length - 1]);
+
     g.selectAll('path').data(countries.features)
         .enter().append('path')
         .attr('class', 'country')
         .attr('d', pathGenerator)
-        .attr("fill", d => colorScale(d.properties.GDP))
+        .attr("fill", d => colorScale(colorValueOECD(d)))
         .append("title")
             .text(d => d.properties.name)
 //        .style('fill', )
